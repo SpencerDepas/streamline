a56046e2f3e91a46367a1d58396eaba2a71887de
Spencer Zeke Depas <spencerdepas@gmail.com>
2018-06-02 14:04:34 +0800

    first commit

---
+++ app/src/main/java/gundam/gunpla/com/streamline/MainActivity.java
@@ -0,0 +1,14 @@
+package gundam.gunpla.com.streamline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hello world
+    }
+}
